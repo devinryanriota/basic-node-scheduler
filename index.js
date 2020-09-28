73046d58193e16dcd857afb56dfee8f16efd5a30
Devin Ryan Riota <devinryanriota@gmail.com>
2020-09-28 22:10:52 +0700

    environment variable port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cron = require('node-cron')
 const FetchService = require('./service/FetchService')
 
-const PORT = 3030
+const PORT = process.env.PORT || 3000
 
 //schedule
 const EVERY_1_MIN_CRON = '* * * * *'
